d64518df12af5dfd489197a6f91d3d44cfd1589a
JustinWested <justwested@gmail.com>
2023-05-03 22:22:34 -0700

    Scroll drawing functionality complete

---
+++ js/main.js
@@ -0,0 +1,21 @@
+const observer = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+      const svg = entry.target;
+      const paths = svg.querySelectorAll('path');
+      const lines = svg.querySelectorAll('line');
+      const polylines = svg.querySelectorAll('polyline');
+  
+      if (entry.isIntersecting) {
+        paths.forEach(path => path.classList.add('draw-animation'));
+        lines.forEach(line => line.classList.add('draw-animation'));
+        polylines.forEach(polyline => polyline.classList.add('draw-animation'));
+        return; // if we added the class, exit the function
+      }
+    });
+  }, {
+    rootMargin: '-15% 0px -85% 0px'
+  });
+  
+  const svgs = document.querySelectorAll('.svg-wrapper svg');
+  svgs.forEach(svg => observer.observe(svg));
+  
